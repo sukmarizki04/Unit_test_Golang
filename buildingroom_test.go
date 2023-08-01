44392b79dd76ace2be0e6c1b5c0b6627ca20bf5b
sukmarizki04 <sukmarizki04@gmail.com>
2023-08-01 21:59:14 +0700

    buildingroom

---
+++ buildingroom_test.go
@@ -0,0 +1,18 @@
+package helper
+
+import "testing"
+
+var (
+	kubus              Kubus   = Kubus{4}
+	volumeSeharusnya   float64 = 64
+	luasSeharusnya     float64 = 96
+	kelilingSeharusnya float64 = 48
+)
+
+func TestHitungVolume(t *testing.T) {
+	t.Logf("Volume : %.2f", kubus.Volume())
+
+	if kubus.Volume() != volumeSeharusnya {
+		panic("Salah harusnya %2.f")
+	}
+}
