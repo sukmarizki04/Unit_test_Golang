41c0f68a2e8693bff8d7461127c013cf9d10d320
sukmarizki04 <sukmarizki04@gmail.com>
2023-08-06 16:58:32 +0700

    beginner

--- hello_world_test.go
+++ hello_world_test.go
@@ -3,8 +3,27 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+func TestHelloWorldRequire(t *testing.T) {
+
+	result := HelloWorld(" Sukma")
+	var err = "Program not found"
+	require.Equal(t, "Hello Sukma", result, err)
+	fmt.Println("Assert Require Done")
+}
+
+func TestHelloWorldAssertion(t *testing.T) {
+
+	result := HelloWorld(" Sukma")
+	var err = "Program not found"
+	assert.Equal(t, "Hello Sukma", result, err)
+	fmt.Println("TestHelloWOrldAssertion Done")
+
+}
 func TestHelloWorldAdi(t *testing.T) {
 	uji := HelloWorld(" Sukma")
 
@@ -18,9 +37,9 @@ func TestHelloWorldAdi(t *testing.T) {
 func TestHelloWorldSukma(t *testing.T) {
 	uji := HelloWorld(" Sukma")
 
-	if uji != "Hello Sukma" {
+	if uji == "Hello Sukma" {
 		//sedangkan testfailnow akan berhenti ketika menemukan function FailNow
-		t.FailNow()
+		t.Fatal("Result must Be 'He Sukma'")
 	}
 
 	fmt.Println("TestHelloWorldSukma Done")
